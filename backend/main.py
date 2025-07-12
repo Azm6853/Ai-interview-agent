bce13ddc10616121014b8af362be1b8f3e8bf8d2
Azm6853 <123222309+Azm6853@users.noreply.github.com>
2025-07-12 19:50:43 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ import uuid
 
 from backend.resume_parser import parse_resume
 
-from utils.role_matcher import match_role
+from role_matcher import match_role
+
 from agent_engine import generate_question
 from scoring import score_answer
 
